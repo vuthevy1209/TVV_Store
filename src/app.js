973a528be593ce2acc641e3f736710f5fc09d9d8
html <22120436@student.hcmus.edu.vn>
2024-11-30 08:21:30 +0700

    Move previously created folder to src directory

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes');
 var usersRouter = require('./routes/users');
-const {connect} = require("./config/database/index");
+const {connect} = require("./config/database");
 
 var app = express();
 
