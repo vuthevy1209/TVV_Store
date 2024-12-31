780a91f6cc3bffef4e4ed70f707e895f0eca8614
Tuyen572004 <113967778+Tuyen572004@users.noreply.github.com>
2024-12-31 07:27:44 +0700

    update: node-cron to retrive cancelled product to inventory

--- src/utils/node-cron.js
+++ src/utils/node-cron.js
@@ -2,25 +2,37 @@ const cron = require('node-cron');
 const { Op } = require('sequelize');
 const Order = require('../modules/order/models/order');
 const orderEnums = require('../modules/order/enums/order.enums');
+const productService = require('../modules/product/services/product.services');
+const OrderItems = require('../modules/order/models/orderItem');
 
 // Cron job to mark expired orders as failed
 cron.schedule('* * * * *', async () => { // Run this every hour
-    try {
-        const expiredOrders = await Order.findAll({
-            where: {
-                status: orderEnums.OrderStatusEnum.PENDING.value,
-                expired_at: {
-                    [Op.lte]: new Date() // Orders that have passed the expired_at time
-                }
+    const expiredOrders = await Order.findAll({
+        where: {
+            status: orderEnums.OrderStatusEnum.PENDING.value,
+            expired_at: {
+                [Op.lte]: new Date() // Orders that have passed the expired_at time
             }
-        });
-
-        for (let order of expiredOrders) {
-            // Mark the order as failed
-            await order.update({ status: orderEnums.OrderStatusEnum.CANCELLED.value, expired_at: null });
-            console.log(`Order ${order.id} has been marked as failed`);
         }
-    } catch (err) {
-        console.error('Error marking expired orders as failed:', err);
-    }
+    });
+
+    return await sequelize.transaction(async (transaction) => {
+        for (let order of expiredOrders) {
+            try {
+                // Mark the order as failed
+                await order.update({ status: orderEnums.OrderStatusEnum.CANCELLED.value, expired_at: null });
+                // Return the product quantity
+                const orderItems = await OrderItems.findAll({
+                    where: { order_id: order.id }
+                });
+                await Promise.all(orderItems.map(async (orderItem) => {
+                    await productService.updateProductInventory(orderItem.product_id, -orderItem.quantity, { transaction });
+                }));
+                console.log(`Order ${order.id} has been marked as failed`);
+            }
+            catch (err) {
+                console.error(`Error marking order ${order.id} as failed:`, err);
+            }
+        }
+    });
 });
